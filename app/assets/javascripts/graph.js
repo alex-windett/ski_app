740b02c053ab0461c3d2daf22b8963e8322a4d2f
Alex Windett <alexwindett@hotmail.com>
2014-09-08 21:28:14 +0100

    elevation data mapping to graph

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,84 +1,85 @@
 $(document).ready(function() {
     
-   d3.json('/markers.json', function(error, data){
-    elevation = []
-    data.forEach(function(d){
-        // console.log(d.elevation)
-        elevation.push(d.elevation)
-        })
-    console.log(elevation)
+    d3.json('/markers.json', function(error, data){
+        elevation = []
+        data.forEach(function(d){
+            // console.log(d.elevation)
+            elevation.push(d.elevation)
+            })
+        console.log(elevation)
 
-    data = elevation,
-    // data = [1, 3 ,5 ,6 ,7 ,7 , 12]
-    console.log(data)
-    w = 600,
-    h = 300,
-    margin = 20,
-    y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
-    x = d3.scale.linear().domain([0, data.length]).range([0 + margin, w - margin])
+// data = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 200000, 5, 7],
+var data = elevation
+console.log(data)
+w = 400,
+h = 200,
+margin = 20,
+y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
+x = d3.scale.linear().domain([0, data.length]).range([0 + margin, w - margin])
 
-    var vis = d3.select(".graph")
-        .append("svg:svg")
-        .attr("width", w)
-        .attr("height", h)
-     
-    var g = vis.append("svg:g")
-        .attr("transform", "translate(0, 200)");
+var vis = d3.select(".graph")
+    .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h)
+ 
+var g = vis.append("svg:g")
+    .attr("transform", "translate(0, 200)");
 
-    var line = d3.svg.line()
-        .x(function(d,i) { return x(i); })
-        .y(function(d) { return -1 * y(d); })
+var line = d3.svg.line()
+    .x(function(d,i) { return x(i); })
+    .y(function(d) { return -1 * y(d); })
 
-    g.append("svg:path").attr("d", line(data));
+g.append("svg:path").attr("d", line(data));
 
-    g.append("svg:line")
-        .attr("x1", x(0))
-        .attr("y1", -1 * y(0))
-        .attr("x2", x(w))
-        .attr("y2", -1 * y(0))
-     
-    g.append("svg:line")
-        .attr("x1", x(0))
-        .attr("y1", -1 * y(0))
-        .attr("x2", x(0))
-        .attr("y2", -1 * y(d3.max(data)))
+g.append("svg:line")
+    .attr("x1", x(0))
+    .attr("y1", -1 * y(0))
+    .attr("x2", x(w))
+    .attr("y2", -1 * y(0))
+ 
+g.append("svg:line")
+    .attr("x1", x(0))
+    .attr("y1", -1 * y(0))
+    .attr("x2", x(0))
+    .attr("y2", -1 * y(d3.max(data)))
 
-    g.selectAll(".xLabel")
-        .data(x.ticks(5))
-        .enter().append("svg:text")
-        .attr("class", "xLabel")
-        .text(String)
-        .attr("x", function(d) { return x(d) })
-        .attr("y", 0)
-        .attr("text-anchor", "middle")
-     
-    g.selectAll(".yLabel")
-        .data(y.ticks(4))
-        .enter().append("svg:text")
-        .attr("class", "yLabel")
-        .text(String)
-        .attr("x", 0)
-        .attr("y", function(d) { return -1 * y(d) })
-        .attr("text-anchor", "right")
-        .attr("dy", 4)
+g.selectAll(".xLabel")
+    .data(x.ticks(5))
+    .enter().append("svg:text")
+    .attr("class", "xLabel")
+    .text(String)
+    .attr("x", function(d) { return x(d) })
+    .attr("y", 0)
+    .attr("text-anchor", "middle")
+ 
+g.selectAll(".yLabel")
+    .data(y.ticks(4))
+    .enter().append("svg:text")
+    .attr("class", "yLabel")
+    .text(String)
+    .attr("x", 0)
+    .attr("y", function(d) { return -1 * y(d) })
+    .attr("text-anchor", "right")
+    .attr("dy", 4)
 
-    g.selectAll(".xTicks")
-        .data(x.ticks(5))
-        .enter().append("svg:line")
-        .attr("class", "xTicks")
-        .attr("x1", function(d) { return x(d); })
-        .attr("y1", -1 * y(0))
-        .attr("x2", function(d) { return x(d); })
-        .attr("y2", -1 * y(-0.3))
-     
-    g.selectAll(".yTicks")
-        .data(y.ticks(4))
-        .enter().append("svg:line")
-        .attr("class", "yTicks")
-        .attr("y1", function(d) { return -1 * y(d); })
-        .attr("x1", x(-0.3))
-        .attr("y2", function(d) { return -1 * y(d); })
-        .attr("x2", x(0))
+  g.selectAll(".xTicks")
+    .data(x.ticks(5))
+    .enter().append("svg:line")
+    .attr("class", "xTicks")
+    .attr("x1", function(d) { return x(d); })
+    .attr("y1", -1 * y(0))
+    .attr("x2", function(d) { return x(d); })
+    .attr("y2", -1 * y(-0.3))
+ 
+g.selectAll(".yTicks")
+    .data(y.ticks(4))
+    .enter().append("svg:line")
+    .attr("class", "yTicks")
+    .attr("y1", function(d) { return -1 * y(d); })
+    .attr("x1", x(-0.3))
+    .attr("y2", function(d) { return -1 * y(d); })
+    .attr("x2", x(0))
 
-    });
+  });
+
 });

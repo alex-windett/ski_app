ce64980a3e902a565c35df2914c223268d1ee80b
Alex Windett <alexwindett@hotmail.com>
2014-09-08 11:56:38 +0100

    heroku

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,6 +1,6 @@
 var data = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 7],
-w = 400,
-h = 200,
+w = 600,
+h = 300,
 margin = 20,
 y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
 x = d3.scale.linear().domain([0, data.length]).range([0 + margin, w - margin])
